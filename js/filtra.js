d763840f5724cf046d5cf5c3fd5b5e677e678433
Tiago Moraes <tflavio@gmail.com>
2019-11-04 23:05:41 -0300

    Criação de remoção de itens da tabela / filtro

---
+++ js/filtra.js
@@ -0,0 +1,28 @@
+
+// No caso de não haver nada digitado, percorreremos todos os pacientes e removeremos a classe com um segundo for:
+
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+            if (!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
